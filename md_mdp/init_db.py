cc5359c58a3367f6e535d531cdb7c8415c83c837
Zhuyi Xue <zhuyi.xue@utoronto.ca>
2011-10-17 17:57:50 -0400

    written values to the database. Created two tables

--- md_mdp/init_db.py
+++ md_mdp/init_db.py
@@ -1,10 +1,100 @@
+import os
+import time
+import random
+
 import sqlalchemy as sqlmy
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float
 
-engine = sqlmy.create_engine('sqlite:////tmp/md_mdp.db', echo=True)
-
-print dir(sqlmy)
 Base = declarative_base()
 
-class 
+class Variables(Base):
+    __tablename__ = 'Variables'
+
+    id = Column(Integer, primary_key=True)
+    ref_t = Column(String)
+    gen_temp = Column(String)
+
+    def __init__(self, ref_t, gen_temp):
+        self.ref_t = ref_t
+        self.gen_temp = gen_temp
+
+    def __repr__(self):
+        return "<id: %d ref_t: %s>" % (id, ref_t)
+
+class Exchanges(Base):
+    __tablename__ = "Exchanges"
+    
+    exchangeid = Column(Integer, primary_key=True)
+    potential_energy = Column(Float)
+    original = Column(Integer)
+    to_change = Column(Integer)
+    changed = Column(Integer)
+    global_temps = Column(String)
+    date = Column(String)
+    
+    def __init__(self, potential_energy, original, to_change, changed, global_temps, date): # 
+        self.potential_energy = potential_energy
+        self.original = original
+        self.to_change = to_change
+        self.changed = changed
+        self.global_temps = global_temps
+        self.date = date
+
+    def __repr__(self):
+        return "<%d %f %s %s %r>" % (potential_energy, to_change, changed, date)
+
+def init_db(ref_ts):
+    for t in ref_ts:
+        gen_temp = '{t}\t{t}'.format(t=t)
+        var = Variables(ref_t=t, gen_temp=gen_temp)
+        ss.add(var)
+
+def exchange_or_not(original, to_change):
+    import random
+    if random.randint(0, 1) == 1:
+        changed = to_change
+    else:
+        changed = original
+    return changed
+
+
+if __name__ == "__main__":
+    engine = sqlmy.create_engine('sqlite:////tmp/md_mdp.db', echo=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    ss = Session()
+
+    initial_temps = range(200, 301, 10)
+
+    if not os.path.exists('/tmp/md_mdp.db'):
+        init_db(initial_temps)
+
+    gt = [str(i) for i in initial_temps]
+    global_temps = ' '.join(gt)
+
+    potential_energy = 12.003
+    original = None
+    to_change = None
+    changed = None
+    date = time.ctime()
+
+    for i in range(10):
+        ex = Exchanges(potential_energy, original, to_change, changed, global_temps, date)
+        ss.add(ex)
+        k = random.randint(0,10)
+        original = gt[k]
+        if k == 10:
+            to_change = gt[0]
+        else:
+            to_change = gt[k+1]
+        changed = exchange_or_not(original, to_change)
+        gt[k] = changed
+        global_temps = ' '.join(gt)
+        
+    ss.commit()
+
+    # for r in ss.query(Variables).all():
+    #     print r.ref_t, r.gen_temp
--- md_mdp/prepare_sql.py
+++ md_mdp/prepare_sql.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 def get_md_mdp_parameters(mdpf):                            # without temperatures
-    variables, md_mdp_para = {}, {}
+    varied_para, const_para = {}, {}
     with open(mdpf, 'r') as inf:
         for line in inf:
             ll = line.strip()
@@ -10,31 +10,39 @@ def get_md_mdp_parameters(mdpf):                            # without temperatur
                     pass
                 elif ll.startswith('@'):
                     sl = ll.strip('@').split('=')
-                    variables[sl[0].strip()] = sl[1].strip().split(',')
+                    varied_para[sl[0].strip()] = sl[1].strip().split(',')
                 else:
                     sl = ll.split('=')
-                    md_mdp_para[sl[0].strip()] = sl[1].strip()
-    return variables, md_mdp_para
+                    const_para[sl[0].strip()] = sl[1].strip()
+    return varied_para, const_para
 
-def create_table_cmd(db, table_name, variables, md_mdp_para):
-    c = db.cursor()
+def create_cmd_for_init_db(db, varied_para, const_para, table_name='md_mdp_para'):
+    vkeys = varied_para.keys()
+    vqm = ','.join(['?'] * len(vkeys))                      # varied_question_marks
+    vkeys_cmd_format = 'CREATE TABLE {0} ({1})'.format(table_name, vqm)
+    vkeys_cmd = (vkeys_cmd_format, vkeys)
 
-    vkeys = sorted(variables.keys())
-    mkeys = sorted(md_mdp_para.keys())
+    vvalues = zip(*[varied_para[k] for k in vkeys])         # a list of tuples/rows
+    vvalues_cmd_format = 'INSERT INTO {0} VALUES {1}'.format(table_name, vqm)
+    vvalues_cmd = (vvalues_cmd_format, vvalues)
+
+    from pprint import pprint as pp
+    pp(vkeys_cmd)
+    pp(vvalues_cmd)
+
+    import sys
+    sys.exit
+    mkeys = sorted(const_para.keys())
     headers = ','.join((['"{k}" text'.format(k=k) for k in vkeys] +
                         ['"{k}" text'.format(k=k) for k in mkeys]))
-    cmd = 'CREATE TABLE {table_name} ({headers})'.format(
-        table_name=table_name, headers=headers)
 
     # NEED CODE TO CHECK IF LALA HAS ALREADY EXISTED OR NOT
-    c.execute(cmd)                                          # create table
+    # c.execute(cmd)                                          # create table
 
-    vvalues = [variables[k] for k in vkeys]
-    mvalues = [md_mdp_para[k] for k in mkeys]
+    vvalues = [varied_para[k] for k in vkeys]
+    mvalues = [const_para[k] for k in mkeys]
     values = ','.join(['"{v}"'.format(v=v) for v in vvalues] +
                       ['"{v}"'.format(v=v) for v in mvalues])
-    from pprint import pprint as pp
-    pp(values)
     # for t in 
     #     cmd = 'INSERT INTO "{table_name}" values({t}'.format(
     #             table_name=table_name, t=t)
@@ -44,10 +52,17 @@ def create_table_cmd(db, table_name, variables, md_mdp_para):
     # db.commit()
     # c.close()
 
+def init_db(db_name, varied_para, const_para, table_name='const_para'):
+    db = sqlite3.connect(db_name)
+    with closing(db) as db:
+        db.execute(cmd)
+        db.commit()
+
 if __name__ == "__main__":
     from pprint import pprint as pp
 
-    variables, md_mdp_para = get_md_mdp_parameters('md.mdp')
-    db = sqlite3.connect('./example.db')
+    varied_para, const_para = get_md_mdp_parameters('md.mdp')
+    # pp(varied_para)
+    # pp(const_para)
 
-    create_table_cmd(db, 'lala', variables, md_mdp_para)
+    create_cmd_for_init_db('db', varied_para, const_para)
