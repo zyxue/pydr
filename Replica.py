a8bc9a214beddeace40dae80a55686c188032910
Zhuyi Xue <zhuyi.xue@utoronto.ca>
2011-10-25 18:22:40 -0400

    deleted redundant directories in ../sq1e

--- Replica.py
+++
@@ -1,9 +0,0 @@
-class Replica(object):
-    def __init__(self, potential_energy, current_temp):
-        self.potential_energy = potential_energy
-        self.current_temp = current_temp
-
-    def to_change(self):
-        # find a way to calc to_change based on current_temp
-        pass
-
--- trial_configobj.py
+++
@@ -1,6 +0,0 @@
-from configobj import ConfigObj
-import validate
-
-config = ConfigObj('pydr_dc/config.ini.example')
-
-print config['job']
